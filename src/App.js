099276d1ef92b8e0ed7ae308047a2cec37e7947f
Ian Lessa <ian.lessa1@gmail.com>
2022-05-06 19:41:29 +0200

    split basic components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import "./categories.styles.scss"
+import Directory from "./components/directory/directory.component";
 
 const App = () => {
-
   const categories = [
     {
       "id": 1,
@@ -31,19 +30,7 @@ const App = () => {
   ];
 
   return (
-    <div className="categories-container">
-      {categories.map(({title, id, imageUrl}) => (
-        <div  key={id} className="category-container">
-          <div className="background-image" style={{
-            backgroundImage: `url(${imageUrl})`
-          }} />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Directory categories={categories} />
   );
 }
 
